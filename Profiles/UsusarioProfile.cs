5d8ea1d5cfda1b070728272a5e115adf40f22d05
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2024-04-18 22:48:08 -0300

    first commit

---
+++ Profiles/UsusarioProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Profiles;
+
+public class UsusarioProfile : Profile
+{
+    public UsusarioProfile()
+    {
+        CreateMap<CreateUsuarioDto, Usuario>();
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(Usuario usuario)
+        {
+            Claim[] claims = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id),
+                new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("J!cc%7t98!GnNFvQ$WY2z#SaYbYEyBVT^sty6A*@"));
+
+            var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+                (
+                    expires: DateTime.Now.AddMinutes(10),
+                    claims: claims,
+                    signingCredentials: signingCredentials
+                );
+        
+            return new JwtSecurityTokenHandler().WriteToken(token); 
+        }
+    }
+}
